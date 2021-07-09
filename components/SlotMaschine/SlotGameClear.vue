29f388be1448173fd882d7f97bd1f530235b5e82
kazutakanakamura711 <a711st777@gmail.com>
2021-07-09 20:43:22 +0900

    機能追加

---
+++ components/SlotMaschine/SlotGameClear.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <v-alert outlined color="purple">
+      <div class="text-h6">ClearData</div>
+      <div>
+        Do you want to delete the game history?<br />
+        If you delete it, you can play from the beginning.
+        <v-btn @click="gameClear">OK</v-btn>
+      </div>
+    </v-alert>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    gameClear() {
+      localStorage.clear()
+      location.reload()
+    }
+  }
+}
+</script>
+
+<style>
+</style>
