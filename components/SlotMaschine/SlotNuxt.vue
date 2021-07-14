ee3a7c0c4734beae6f7081bc6c1c62f2b74169c8
kazutakanakamura711 <a711st777@gmail.com>
2021-07-14 15:53:31 +0900

    機能追加

--- components/SlotMaschine/SlotNuxt.vue
+++ components/SlotMaschine/SlotNuxt.vue
@@ -5,7 +5,7 @@
     </div>
     <transition name="transit">
       <div v-show="addGame">
-        <v-alert color="yellow">+{{ addGame }}G</v-alert>
+        <v-alert color="yellow">+{{ addGame }}Game</v-alert>
       </div>
     </transition>
   </div>
